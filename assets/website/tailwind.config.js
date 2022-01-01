ceeca13462ce83878a780e40ec5ea05a2577d060
Roland <rolandgolla@gmail.com>
2022-01-01 22:59:32 +0100

    Add tailwind css
    to assert folder and link build in base template

---
+++ assets/website/tailwind.config.js
@@ -0,0 +1,14 @@
+const {content} = require("tailwindcss/lib/plugins");
+module.exports = {
+    purge: {
+        enabled: true,
+        content: ['./../../templates/**/*.twig'],
+    },
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
